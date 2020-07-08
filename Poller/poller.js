baec47044698c9fbfb65350f2c7c3715065eeba2
camac <cesar.alejandro.macias@gmail.com>
2020-07-08 00:37:44 -0500

    test

--- Poller/poller.js
+++ Poller/poller.js
@@ -1,29 +1,30 @@
+/*jslint node: true */
 'use strict';
 
-const poller = require('./snmp.js');
-const args = require('minimist')(process.argv.slice(2));
+const poller = require( './snmp.js' );
+const args = require( 'minimist' )( process.argv.slice( 2 ) );
 
 async function run() {
     let expect = ["hosts", "options", "community"];
-    poller.read_config(args.config, expect)
-        .then(async (conf) => {
-            let func_array = conf.hosts.map(async (target) => {
+    poller.read_config( args.config, expect )
+        .then( async (conf) => {
+            let func_array = conf.hosts.map( async (target) => {
                 if (conf.time)
                     conf.pollertime = Date.now() / 1000;
-                const inh = ("inh_oids" in conf) ? await poller.get_oids(target, conf.community, conf.options, conf.inh_oids) : false;
+                const inh = ("inh_oids" in conf) ? await poller.get_oids( target, conf.community, conf.options, conf.inh_oids ) : false;
                 let result = [];
                 if ("table" in conf) {
                     for (const table of conf.table) {
                         if ("options" in table) {
                             if (!("measurement" in table.options)) {
-                                console.error(new Error("no declarado measurement"));
+                                console.error( new Error( "no declarado measurement" ) );
                                 continue;
                             }
                         } else {
-                            console.error(new Error("no ha declarado options"));
+                            console.error( new Error( "no ha declarado options" ) );
                             continue;
                         }
-                        const part = await poller.get_table(target, conf.community, conf.options, table.oids, conf.maxRepetitions, conf.limit);
+                        const part = await poller.get_table( target, conf.community, conf.options, table.oids, conf.maxRepetitions, conf.limit );
                         for (let k in part) {
                             let doc = part[k];
                             if ("index" in table.options && table.options.index) doc.tag.index = k;
@@ -33,27 +34,28 @@ async function run() {
                             if (inh)
                                 for (let i in inh)
                                     doc.tag[i] = inh[i];
-                            console.log(JSON.stringify(doc));
+                            console.log( JSON.stringify( doc ) );
                         }
-                        result.push(part);
+                        result.push( part );
                     }
                 }
                 if ("oids_get" in conf && "measurement" in conf) {
-                    const doc =  await poller.get_all(target, conf.community, conf.options, conf.oids_get);
+                    const doc = await poller.get_all( target, conf.community, conf.options, conf.oids_get );
                     doc.measurement_name = conf.measurement;
                     doc.tag.agent_host = target;
                     if ("pollertime" in conf) doc.pollertime = conf.pollertime;
                     if (inh)
                         for (let i in inh) doc.tag[i] = inh[i];
-                    console.log(JSON.stringify(doc));
-                    result.push(doc);
+                    console.log( JSON.stringify( doc ) );
+                    result.push( doc );
                 }
                 return result;
-            });
-            await Promise.all(func_array);
-        })
-        .catch(error => console.error(error.toString()));
+            } );
+            await Promise.all( func_array );
+        } )
+        .catch( error => console.error( error.toString() ) );
 }
 
-if ("config" in args)
+if ("config" in args) {
     run();
+}
