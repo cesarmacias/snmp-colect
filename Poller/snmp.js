b77dec1f232ec2d66edd1f9a4a17550654c430a5
camac <cesar.alejandro.macias@gmail.com>
2020-07-09 02:26:32 -0500

    Add support snmpwalk to poller-cm.js

--- Poller/snmp.js
+++ Poller/snmp.js
@@ -1,82 +1,121 @@
 /*jslint node: true */
 'use strict';
 
-const snmp = require("net-snmp");
-const fs = require("fs");
-const async = require("async");
-const addr = require("ip-address");
+import snmp from "net-snmp";
+
+import fs from "fs";
+
+import async from "async";
+
+import addr from "ip-address";
 
 /*
 Funcion para convetir el valor recibido en IPv4
 */
 async function addr_convert(value) {
-    return new Promise((resolve, reject) => {
+    return new Promise( (resolve, reject) => {
         let ipv4;
         if (typeof value === 'string') {
-            ipv4 = new addr.Address4(value);
+            ipv4 = new addr.Address4( value );
             if (ipv4.isValid()) {
-                resolve(ipv4.address);
+                resolve( ipv4.address );
             } else {
-                value = value.replace(/[:.\s]/g, '');
+                value = value.replace( /[:.\s]/g, '' );
                 let reg = /^[0-9a-fA-F]{8}$/g;
-                if (reg.test(value)) {
-                    ipv4 = new addr.Address4.fromHex(value);
+                if (reg.test( value )) {
+                    ipv4 = new addr.Address4.fromHex( value );
                     if (ipv4.isValid())
-                        resolve(ipv4.address);
-                    else reject("Not IPv4");
-                } else reject("Not IPv4");
+                        resolve( ipv4.address );
+                    else reject( "Not IPv4" );
+                } else reject( "Not IPv4" );
             }
         } else if (typeof value === 'number') {
-            ipv4 = new addr.Address4.fromInteger(value);
+            ipv4 = new addr.Address4.fromInteger( value );
             if (ipv4.isValid()) {
-                resolve(ipv4.address);
-            } else reject("Not IPv4");
+                resolve( ipv4.address );
+            } else reject( "Not IPv4" );
         }
-        reject("Error Type");
-    });
+        reject( "Error Type" );
+    } );
+}
+
+/*
+Funcion para tratar/modificar el valor recibido
+*/
+async function vb_transform(vb, oid) {
+    return new Promise( async (resolve) => {
+        let value = vb.value;
+        if (vb.type === snmp.ObjectType.OctetString) {
+            value = vb.value.toString();
+            if ("type" in oid && oid.type === "hex")
+                value = vb.value.toString( "hex" );
+            if ("type" in oid && oid.type === "regex") {
+                if ("regex" in oid && "map" in oid) {
+                    let arr = value.match( new RegExp( oid.regex ) );
+                    if (arr) {
+                        value = {};
+                        for (let i = 1, len = oid.map.length; i <= len; i++)
+                            value[oid.map[i - 1]] = arr[i];
+                    }
+                }
+            }
+        } else if (vb.type === snmp.ObjectType.Counter64) {
+            value = 0;
+            for (let x of vb.value.values()) {
+                value *= 256;
+                value += x;
+            }
+        }
+        let resp = value;
+        if ("conversion" in oid) {
+            resp = oid.conversion === "ipv4" ? await addr_convert( value ) : value;
+        }
+        resolve( resp );
+    } );
 }
 
 /* Funcion para procesar los datos obtenidos de un snmpwalk a un dispositivo, los valores los asocia a "field" o "tag"
  *  * */
 async function feedCb(varbinds) {
     let self = this;
-    for (let i = 0; i < varbinds.length; i++) if (snmp.isVarbindError(varbinds[i])) {
-        console.error(snmp.varbindError(varbinds[i]).toString);
-    } else {
-        let index = varbinds[i].oid.substring(self.mib.oid.length + 1);
-        let value, type;
-        if (varbinds[i].type === snmp.ObjectType.OctetString) {
-            value = "type" in self.mib && self.mib.type === "hex" ? varbinds[i].value.toString("hex") : varbinds[i].value.toString();
-        } else if (varbinds[i].type === snmp.ObjectType.Counter64) {
-            value = 0;
-            for (let x of varbinds[i].value.values()) {
-                value *= 256;
-                value += x;
-            }
-        } else value = varbinds[i].value;
-        if ("conversion" in self.mib && self.mib.conversion === "ipv4") value = await addr_convert(value);
-        type = "tag" in self.mib && self.mib.tag ? "tag" : "field";
-        if (!(index in self.resp)) self.resp[index] = {};
-        if (!("tag" in self.resp[index])) self.resp[index].tag = {};
-        if (!("field" in self.resp[index])) self.resp[index].field = {};
-        self.resp[index][type][self.mib.name] = value;
-    }
+    for (let i = 0; i < varbinds.length; i++)
+        if (snmp.isVarbindError( varbinds[i] )) {
+            console.error( snmp.varbindError( varbinds[i] ).toString );
+        } else {
+            let index = varbinds[i].oid.substring( self.mib.oid.length + 1 );
+            let value, type;
+            if (varbinds[i].type === snmp.ObjectType.OctetString) {
+                value = "type" in self.mib && self.mib.type === "hex" ? varbinds[i].value.toString( "hex" ) : varbinds[i].value.toString();
+            } else if (varbinds[i].type === snmp.ObjectType.Counter64) {
+                value = 0;
+                for (let x of varbinds[i].value.values()) {
+                    value *= 256;
+                    value += x;
+                }
+            } else value = varbinds[i].value;
+            if ("conversion" in self.mib && self.mib.conversion === "ipv4") value = await addr_convert( value );
+            type = "tag" in self.mib && self.mib.tag ? "tag" : "field";
+            if (!(index in self.resp)) self.resp[index] = {};
+            if (!("tag" in self.resp[index])) self.resp[index].tag = {};
+            if (!("field" in self.resp[index])) self.resp[index].field = {};
+            self.resp[index][type][self.mib.name] = value;
+        }
 }
 
 /* Funcion para leer la configuracion para el proceso de poleo snmp, fuente un archivo JSON
  *  * */
 async function read_config(file, inh) {
-    return new Promise((resolve, reject) => {
+    return new Promise( (resolve, reject) => {
         let config;
         try {
-            let rawdat = fs.readFileSync(file, 'utf8');
-            config = JSON.parse(rawdat);
+            let rawdat = fs.readFileSync( file, 'utf8' );
+            config = JSON.parse( rawdat );
         } catch (error) {
-            reject(error);
+            reject( error );
         }
-        inh.forEach((k) => {
-            if (!(k in config)) reject(new Error("Falta el parametro: " + k));
-        });
+        inh.forEach( (k) => {
+            if (!(k in config)) reject( new Error( "Falta el parametro: " + k ) );
+        } );
         if ("options" in config)
             if ("version" in config.options)
                 if (config.options.version === "1")
@@ -86,72 +125,72 @@ async function read_config(file, inh) {
         if (!("maxRepetitions" in config)) config.maxRepetitions = 20;
         if (!("limit" in config)) config.limit = 3;
         if (!("time" in config)) config.time = true;
-        resolve(config);
-    });
+        resolve( config );
+    } );
 }
 
-/* Funcion para obtener datos los datos tipo tabla (indice compartido) por SNMP
+/* Funcion para obtener datos tipo tabla (indice compartido) por SNMP
  *  * */
 async function get_table(target, comm, options, oids, max, limit) {
-    return new Promise((resolve, reject) => {
+    return new Promise( (resolve, reject) => {
         let obj = {};
-        let session = snmp.createSession(target, comm, options);
-        async.eachLimit(oids, limit, (oid, callback) => {
-            session.subtree(oid.oid, max, feedCb.bind({mib: oid, resp: obj}), (error) => {
+        let session = snmp.createSession( target, comm, options );
+        async.eachLimit( oids, limit, (oid, callback) => {
+            session.subtree( oid.oid, max, feedCb.bind( {mib: oid, resp: obj} ), (error) => {
                 if (error)
-                    console.error("table|" + target + "|" + oid.oid + "|" + error.toString());
+                    console.error( "table|" + target + "|" + oid.oid + "|" + error.toString() );
                 callback();
-            });
+            } );
         }, function (error) {
             session.close();
             if (error) {
-                console.error(error.toString());
-                reject(new Error("SNMP error host: " + target));
+                console.error( error.toString() );
+                reject( new Error( "SNMP error host: " + target ) );
             }
-            resolve(obj);
-        });
-    });
+            resolve( obj );
+        } );
+    } );
 }
 
 /* Funcion para obtener datos snmpget para ser heredados en las tablas
  *  * */
 async function get_oids(target, comm, options, oids) {
-    return new Promise((resolve) => {
-        let session = snmp.createSession(target, comm, options);
-        session.get(Object.keys(oids), (error, varbinds) => {
+    return new Promise( (resolve) => {
+        let session = snmp.createSession( target, comm, options );
+        session.get( Object.keys( oids ), (error, varbinds) => {
             let resp = {};
             if (error) {
-                console.error("get|" + target + "|oids|" + error.toString());
+                console.error( "get|" + target + "|oids|" + error.toString() );
             } else {
-                resp = varbinds.reduce((vbs, vb) => {
-                    if (!(snmp.isVarbindError(vb))) {
+                resp = varbinds.reduce( (vbs, vb) => {
+                    if (!(snmp.isVarbindError( vb ))) {
                         vbs[oids[vb.oid]] = vb.value;
                         if (vb.type === snmp.ObjectType.OctetString) vbs[oids[vb.oid]] = vb.value.toString();
                     }
                     return vbs;
-                }, {});
+                }, {} );
             }
             session.close();
-            resolve(resp);
-        });
-    });
+            resolve( resp );
+        } );
+    } );
 }
 
 /* Funcion para obtener varios datos por snmpget, desde un array de OIDS
  * * */
 async function get_all(target, comm, options, oids) {
-    return new Promise((resolve, reject) => {
-        let session = snmp.createSession(target, comm, options);
-        let _oids = Object.keys(oids);
+    return new Promise( (resolve, reject) => {
+        let session = snmp.createSession( target, comm, options );
+        let _oids = Object.keys( oids );
         let resp = {};
         resp.tag = {};
         resp.field = {};
-        session.get(_oids, (error, varbinds) => {
+        session.get( _oids, (error, varbinds) => {
             if (error) {
-                reject(error);
+                reject( error );
             } else {
                 for (const vb of varbinds) {
-                    if (!(snmp.isVarbindError(vb))) {
+                    if (!(snmp.isVarbindError( vb ))) {
                         let type = "field";
                         if ("tag" in oids[vb.oid] && oids[vb.oid].tag)
                             type = "tag";
@@ -160,10 +199,10 @@ async function get_all(target, comm, options, oids) {
                         if (vb.type === snmp.ObjectType.OctetString) {
                             value = vb.value.toString();
                             if ("type" in oids[vb.oid] && oids[vb.oid].type === "hex")
-                                value = vb.value.toString("hex");
+                                value = vb.value.toString( "hex" );
                             if ("type" in oids[vb.oid] && oids[vb.oid].type === "regex") {
                                 if ("regex" in oids[vb.oid] && "map" in oids[vb.oid]) {
-                                    let arr = value.match(new RegExp(oids[vb.oid].regex));
+                                    let arr = value.match( new RegExp( oids[vb.oid].regex ) );
                                     if (arr) {
                                         value = {};
                                         for (let i = 1, len = oids[vb.oid].map.length; i <= len; i++)
@@ -182,16 +221,60 @@ async function get_all(target, comm, options, oids) {
                         resp[type][name] = value;
                     }
                 }
-                resolve(resp);
+                resolve( resp );
             }
             session.close();
-        });
-    });
+        } );
+    } );
+}
+
+/* Funcion para obtener datos tipo walk por SNMP
+ *  * */
+async function get_bulk(target, comm, options, oids, nonrep, maxrep) {
+    return new Promise( (resolve, reject) => {
+        const nonRepeaters = nonrep || 0;
+        const maxRepetitions = maxrep || 30;
+        let session = snmp.createSession( target, comm, options );
+        let _oids = Object.keys( oids );
+        let resp = {};
+        resp.tag = {};
+        resp.field = {};
+        session.getBulk( _oids, nonRepeaters, maxRepetitions, (error, varbinds) => {
+            if (error) {
+                reject( error );
+            } else {
+                // step through the non-repeaters which are single varbinds
+                for (let i = 0; i < nonRepeaters; i++) {
+                    let vb = varbinds[i];
+                    if (i >= varbinds.length) break;
+                    if (!snmp.isVarbindError( vb )) {
+                        let type = "tag" in oids[vb.oid] && oids[vb.oid].tag ? "tag" : "field";
+                        let name = oids[vb.oid].name;
+                        resp[type][name] = vb_transform( vb, oids[vb.oid] );
+                    }
+                }
+                // then step through the repeaters which are varbind arrays
+                for (let i = nonRepeaters; i < varbinds.length; i++) {
+                    let mib = oids[_oids[i]];
+                    let type = "tag" in mib && mib.tag ? "tag" : "field";
+                    let name = mib.name;
+                    let arr = [];
+                    for (let vb of varbinds[i])
+                        if (!snmp.isVarbindError( vb ))
+                            arr.push( vb_transform( vb, mib ) );
+                    resp[type][name] = arr;
+                }
+                resolve( resp );
+            }
+            session.close();
+        } );
+    } );
 }
 
 module.exports = {
     get_table,
     get_oids,
     get_all,
-    read_config
+    read_config,
+    get_bulk
 };
