013b1ea12e4f48bc68ef6b3aca2692dd09c4008d
camac <cesar.alejandro.macias@gmail.com>
2020-07-09 17:24:18 -0500

    add test input

---
+++ test/readline.json.js
@@ -0,0 +1,14 @@
+/*jslint node: true */
+"use strict";
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+});
+
+rl.on( "line", (line) => {
+    console.log(line);
+} );
