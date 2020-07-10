417ae1203c39d0ae5f936d6927ea5a7157e144fa
cmaciasd <cesar.alejandro.macias@gmail.com>
2020-07-10 10:36:12 -0500

    test table

---
+++ test/snmptree.js
@@ -0,0 +1,57 @@
+/*jslint node: true */
+'use strict';
+
+const snmp = require("net-snmp");
+
+const options = {
+    "maxRepetitions": 3,
+    "community": "public",
+    "snmpOpt": {
+        "version": "2c",
+        "retries": 5,
+        "timeout": 1000,
+        "port": 161
+    }
+};
+
+const hosts = [
+    "10.36.143.179",
+    "10.36.224.168",
+    "10.36.135.64",
+    "10.36.213.51",
+    "10.36.197.98",
+    "10.36.168.94"
+];
+
+const oids = [
+    "1.3.6.1.4.1.4115.1.20.1.1.3.42.1.22",
+    "1.3.6.1.4.1.4115.1.20.1.1.3.42.1.6"
+];
+
+function tablePromisified(host, oid, options) {
+    return new Promise(function (resolve, reject) {
+        const session = snmp.createSession(host, options.community, options.snmpOpt);
+        session.table(oid, options.maxRepetitions, function (error, table) {
+            if (error) {
+                reject(error.toString());
+            } else {
+                resolve(table);
+            }
+        });
+    ]);
+}
+
+async function start() {
+    try {
+        for await (target of hosts) {
+            for await (oid of oids) {
+                let table = await tablePromisified(target, oid, options);
+                console.log(table);
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
