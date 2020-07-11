e8f95470ccfa7de3ca56a1ffa6869e9eb4a77f66
cmaciasd <cesar.alejandro.macias@gmail.com>
2020-07-11 13:33:39 -0500

    snmpwalk with escape of error, when the device has error of sicuence in index

--- Poller/snmp.js
+++ Poller/snmp.js
@@ -238,7 +238,9 @@ async function get_walk(target, comm, options, oids, maxrep, maxIterations) {
             let mib = oids[oid];
             let type = "tag" in mib && mib.tag ? "tag" : "field";
             let value = await streePromisified(session, oid, maxRepetitions, mib, maxIterations).catch((error) => {
-                    console.error(`${target}|${oids[oid].name}|${error.toString()}`);
+                    //console.error(`${target}|${oids[oid].name}|${error.toString()}`);
+                    value = {"tag": {"error": {}}};
+                    value.tag.error[oids[oid].name] = error.toString();
                 }
             );
             if (value && value.length > 0)
