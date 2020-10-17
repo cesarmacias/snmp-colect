002fc8c8034b54fc178dee934c7aaa3a218764a8
cmaciasd <cesar.alejandro.macias@gmail.com>
2020-10-16 23:07:27 -0500

    update libraries add script sql to json lines

---
+++ Poller/sql2ndjson.js
@@ -0,0 +1,43 @@
+/*jslint node: true */
+'use strict';
+
+const mysql = require("mysql");
+const fs = require("fs");
+const argv = require("minimist")(process.argv.slice(2));
+const func = require("./tools.js");
+
+/*
+    FUNCTION TO RED CONFIG FILE - PREPARE THE LOOP FOR SEARCH
+ */
+async function main(confFile) {
+    try {
+        if (fs.existsSync(confFile)) {
+            const strConf = fs.readFileSync(confFile, 'utf8');
+            const config = JSON.parse(strConf);
+            const connection = mysql.createConnection(config.dbOpt);
+            connection.connect();
+            await connection.query(config.sql, (err, rows, fields) => {
+                if (err) throw err;
+                rows.forEach((obj) => {
+                    console.log(JSON.stringify(func.ObjExpand(obj)));
+                });
+            });
+            connection.end();
+        } else {
+            throw ("config file not exists: " + confFile);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+/*
+ STAR PROGRAM
+ */
+if ("config" in argv) {
+    main(argv.config).catch(e => {
+        console.error(e);
+    });
+} else {
+    console.error("Not ARG --config with config file path");
+}
